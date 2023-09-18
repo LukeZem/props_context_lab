577a2108d11a483453c8e71454efc6858c9e90fc
Lucas Zemlin <lzemlin@LABA.PSWAS.LOCAL>
2023-09-18 15:16:37 -0700

    started to correct props errors

--- props_app/src/components/ProductsDisplay/index.jsx
+++ props_app/src/components/ProductsDisplay/index.jsx
@@ -1,20 +1,26 @@
 
-const ProductsDisplay = () => {
+const ProductsDisplay = ({ products }) => {
 
-    const handleProductClick = (id) => {
-        // we have the id of the clicked property
-        
-        // 1. make a new products array (from the old state)
-        // 2. set inCart = true for the clicked product
-        // 3. set state to this new products array
-    }
+  const handleProductClick = (id) => {
+    // we have the id of the clicked property
+
+    // 1. make a new products array (from the old state)
+    let cart = products.map((product) => {
+      if (product.id = id) {
+        product.inCart = true
+      }
+    })
+    // 2. set inCart = true for the clicked product
+    // 3. set state to this new products array (pass in property)
+    
+  }
 
   return (
     <div>
-        {/* we need the products array here */}
-        {products.map((product) => {
-            return <div onClick={() => handleProductClick(product.id)}>product.name</div>
-        })}
+      {/* we need the products array here */}
+      {products.map((product) => {
+        return <div onClick={() => handleProductClick(product.id)}>product.name</div>
+      })}
     </div>
   )
 }
